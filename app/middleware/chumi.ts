405842bb5c0e4bf5b93cf997976de8e8d698321c
top.sun <top.sun@nio.com>
2023-03-09 17:30:08 +0800

    feat: 升级chumi，支持swagger

--- app/middleware/chumi.ts
+++ app/middleware/chumi.ts
@@ -3,6 +3,7 @@ import controller from '../controller';
 
 export default () => {
   return chumi(controller, {
+    swagger: {},
     onSuccess(ctx) {
       if (!ctx.body) {
         ctx.status = 404;
